49af0c7ecd3b80cfce29394e2a5a147b0bc58e81
Simone La Porta <simone@Simones-MacBook-Pro.local>
2024-05-26 18:12:13 +0200

    a

--- lua/options.lua
+++ lua/options.lua
@@ -3,11 +3,9 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
+-- line numbers
+vim.opt.number = true -- Show absolute line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
